674e237accc508d5aa40fd6d77addc1cb68bb376
S-H-YOO <160996806+S-H-YOO@users.noreply.github.com>
2025-03-25 21:01:23 +0900

    Add files via upload

---
+++ Practice/Game/font_rom.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 2024/12/18 14:42:10
+-- Design Name: 
+-- Module Name: font_rom - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity font_rom is
+--  Port ( );
+end font_rom;
+
+architecture Behavioral of font_rom is
+
+begin
+
+
+end Behavioral;
